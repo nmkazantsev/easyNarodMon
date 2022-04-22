945df763299c7470436afd07f991727a11d5a750
nmkazantsev <81066636+nmkazantsev@users.noreply.github.com>
2022-04-22 14:37:50 +0300

    Update basicSend.ino

--- examples/basicSend/basicSend.ino
+++ examples/basicSend/basicSend.ino
@@ -3,20 +3,21 @@
 #include "easyNarodMon.h";
 
 #ifndef STASSID
-#define STASSID "your-ssid"
+#define STASSID "your-ssid" //enret wifi said and pass here
 #define STAPSK  "your-passs"
 #endif
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
-NarodMonSender mySender;
+NarodMonSender mySender; //create a class of sender
 void setup() {
-  mySender.enableDebug();
+  mySender.enableDebug(); //enable serial debugging (default is disabled) 
   Serial.begin(115200);
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
+//usual connection
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -32,9 +33,11 @@ void setup() {
 }
 
 void loop() {
+//add head starts a new request and enters there device id
   mySender.addHead("device-id");
+//add sensor to your request, first param is value, second is id of sensor
   mySender.addSensor("30", "sensor1-id");
   mySender.addSensor("23", "sensor2-id");
-  mySender.sendData();
+  mySender.sendData();//send request 
   delay(300000); // execute once every 5 minutes, don't flood remote service
 }
